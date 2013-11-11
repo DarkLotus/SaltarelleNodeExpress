3afad68c2e09f26117846c21c6d6239988776b32
James Kidd <kklolk@gmail.com>
2013-11-11 14:25:32 +1100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// AssemblyInfo.cs
+// Script#/Libraries/Knockout
+// This source code is subject to terms and conditions of the Apache License, Version 2.0.
+//
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("Script.NodeJS.Express")]
+[assembly: AssemblyDescription("Saltarelle Node.js Express API")]
+[assembly: ScriptAssembly("express")]
+
+[assembly: AssemblyCompany("James Kidd")]
+[assembly: AssemblyProduct("Saltarelle Node.js Express bindings")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: ModuleName("expresssssssss")]
